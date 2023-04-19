792cd9978d921c6d1d1724efce22661d70bb9b0f
stonecat <apink191550@gmail.com>
2023-04-19 16:34:38 +0900

    code

--- React/class/react-ex01/src/App.js
+++ React/class/react-ex01/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { Fragment } from "react";
 import './default.css';
 
 function App() {
+  let a = 3;
+
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
+    <div>
+      <h1>Hello React!!</h1>
+      <p>This is React!</p>
+      <p>This is num : {a}</p>
     </div>
+
+    // <Fragment>
+    //   <h1>Hello React!</h1>
+    // </Fragment>
+
+    // <>
+    //   <h1>Hello React!!</h1>
+    //   <p>This is React!</p>
+    // </>
   );
 }
 
